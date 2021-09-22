4fa38b51b7362c8fa228802f5cca7a58c2b95110
LilianSilveira <lilian.cdsilveira@gmail.com>
2021-09-22 18:45:58 -0300

    separando as bases

--- exemplo_roubo_de_veiculos.R
+++ exemplo_roubo_de_veiculos.R
@@ -95,7 +95,7 @@ base_bruta %>%
   filter(ANO_BO == 2021, NUM_BO == 1347, DELEGACIA_NOME == "16º D.P. VILA CLEMENTINO") %>%
   tibble::view()
 
-# Parece que a coluna status também gera as repetições
+# Parece que a coluna status também gera as repetições. Vamos incluí-la.
 
 base_bruta %>%
   count(ANO_BO, NUM_BO,DELEGACIA_CIRCUNSCRICAO, DELEGACIA_NOME, PLACA_VEICULO, STATUS, ESPECIE, RUBRICA) %>%
@@ -108,7 +108,7 @@ base_bruta %>%
   filter(ANO_BO == 2021, NUM_BO == 1692, DELEGACIA_NOME == "69º D.P. TEOTONIO VILELA") %>%
   tibble::view()
 
-# Parece que a coluna status também gera as repetições
+# Parece que a coluna marca e cor do veículo também geram repetições:
 
 base_bruta %>%
   count(ANO_BO, NUM_BO,DELEGACIA_CIRCUNSCRICAO, DELEGACIA_NOME, PLACA_VEICULO, DESCR_MARCA_VEICULO, DESCR_COR_VEICULO, STATUS, ESPECIE, RUBRICA, DESDOBRAMENTO) %>%
@@ -120,7 +120,7 @@ base_bruta %>%
   filter(ANO_BO == 2021, NUM_BO == 437009, DELEGACIA_NOME == "DELEGACIA ELETRONICA") %>%
   tibble::view()
 
-# Parece que a coluna status também gera as repetições
+# Parece que a cidade do veículo também gera repetições
 
 base_brut5a %>%
   count(ANO_BO, NUM_BO,DELEGACIA_CIRCUNSCRICAO, DELEGACIA_NOME, PLACA_VEICULO, DESCR_MARCA_VEICULO, DESCR_COR_VEICULO, CIDADE_VEICULO, STATUS, ESPECIE, RUBRICA, DESDOBRAMENTO) %>%
@@ -131,4 +131,96 @@ base_brut5a %>%
 
 # Arrumar o nome das colunas ----------------------------------------------
 
-base_nomes_arrumados <- 
+#Verificando as possibilidades em para filtrar:
+
+base_bruta %>%
+  count(PLACA_VEICULO) %>%
+  tibble::view()
+
+base_bruta %>%
+  count(ANO_BO, NUM_BO) %>%
+  tibble::view()
+
+base_bruta %>%
+  count(ESPECIE, DESDOBRAMENTO, RUBRICA, STATUS) %>%
+  tibble::view()
+
+
+#Criando a base arrumada
+
+base_nomes_arrumados_preenchida <- base_bruta %>%
+  # tira espaços e troca por "_", tira acento, simbolos, passa para minúsculo,
+  # se tiver repetição de colunas, ela coloca um índice após a coluna.
+  janitor::clean_names() %>%
+  #remove os carros com id vazio
+  filter(!is.na(placa_veiculo), placa_veiculo != "",
+         !(placa_veiculo %in% c("SEMPLA", "SEMPLAC", "000000", "0000000", "ZZZ0000", "TAMPADA"))) %>%
+  filter(status == "Consumado")
+
+
+#Vamos verificar se parece ok:
+base_nomes_arrumados_preenchida %>%
+  count(placa_veiculo) %>%
+  tibble::view()
+
+# Separando as bases ------------------------------------------------------
+
+# Estratégia:
+# Separar as três bases e montar a base final fazendo joins (left_join)
+# a partir da base de unidade que eu escolher(no caso, veículo)
+# as bases vão ser ocorrencias, crimes e carros
+# dentro de cada base, escolher colunas e remover duplicações
+# juntar as bases pelas chaves correspondentes
+
+# Ocorrências
+
+ocorrencias <- base_nomes_arrumados_preenchida %>%
+  dplyr::select(ano_bo:delegacia_circunscricao,
+                placa_veiculo:ano_fabricacao) %>%
+  dplyr::distinct()
+
+# Veículos
+
+veiculos <- base_nomes_arrumados_preenchida %>%
+  dplyr::select(placa_veiculo:ano_fabricacao) %>%
+  dplyr::distinct()
+
+# Crimes
+
+crimes_passo1 <- base_nomes_arrumados_preenchida %>%
+  dplyr::select(num_bo, ano_bo, delegacia_nome, delegacia_circunscricao,
+                especie:desdobramento) %>%
+  dplyr::distinct() %>%
+  # Cria uma nova coluna e une a info de outras colunas:
+  tidyr::unite(crime_completo, especie, rubrica, desdobramento, sep = " @@@ ")
+
+crimes_passo1 %>% tibble::view()
+
+# Preciso garantir 1 conjunto de crimes só por ecorrência:
+# Opção A - sumarização
+
+crimes_passo2a <- crimes_passo1 %>%
+  dplyr::group_by(num_bo, ano_bo, delegacia_nome, delegacia_circunscricao) %>%
+  dplyr::summarise(
+    todos_os_crimes = paste0(crime_completo, collapse = ", ")
+  ) %>%
+  dplyr::ungroup()
+
+crimes_passo2a %>% tibble::view()
+
+# Opção B - pivot_wider, neste caso não foi uma boa opção.
+
+crimes_passo2b <- crimes_passo1 %>%
+  dplyr::group_by(num_bo, ano_bo, delegacia_nome, delegacia_circunscricao) %>%
+  dplyr::mutate(
+    nome_da_coluna = paste0("crime_", 1:n())
+  ) %>%
+  tidyr::pivot_wider(names_from = nome_da_coluna, values_from = crime_completo)
+
+crimes_passo2b %>% tibble::view()
+
+#Opção C - nest: transformar as linhas da tabela de crimes em uma list-column
+
+
+
+
